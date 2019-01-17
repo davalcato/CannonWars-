ff6c496c48a81875ddb7110b1ce586c4a1e2f44b
davalcato <davalcato@yahoo.com>
2019-01-16 19:51:13 -0800

    added the pegs disappear feature

--- CannonWars/GameScene.swift
+++ CannonWars/GameScene.swift
@@ -22,11 +22,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     
     override func didMove(to view: SKView) {
-        cannon = self.childNode(withName: "cannon") as? SKSpriteNode
-        
+        cannon = (self.childNode(withName: "cannon") as! SKSpriteNode)
+        self.physicsWorld.contactDelegate = self
        
     }
-    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchLocation = touches.first!.location(in: self)
@@ -43,20 +42,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.zPosition = 0
         ball.position = cannon.position
         let angleInRadians = Float(cannon.zRotation)
-        _ = CGFloat(75.0)
+        _ = CGFloat(100.0)
         let _:CGFloat = CGFloat(cosf(angleInRadians)) * speed
         let _:CGFloat = CGFloat(sinf(angleInRadians)) * speed
         ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 75))
-        ball.physicsBody?.collisionBitMask = wallMask | ballMask | pegMask 
-        
-        
+        ball.physicsBody?.collisionBitMask = wallMask | ballMask | pegMask
+        ball.physicsBody?.contactTestBitMask = ball.physicsBody!.collisionBitMask | squareMask
         
     }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
         let percent = touchLocation.x / size.width
         let newAngle = percent * 180 - 180
         cannon.zRotation = CGFloat(newAngle) * CGFloat(Double.pi) / 180.0
     }
-}
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let ball = (contact.bodyA.categoryBitMask == ballMask) ? contact.bodyA : contact.bodyB
+        
+        let other = (ball == contact.bodyA) ? contact.bodyB : contact.bodyA
+        
+        if other.categoryBitMask == pegMask {
+            other.node?.removeFromParent()
+        }
+        else if other.categoryBitMask == squareMask {
+            print("hit square!")
+        }
+        else if other.categoryBitMask == wallMask {
+            print("hit wall!")
+        }
+        else if other.categoryBitMask == ballMask {
+            print("hit ball!")
+        }
+
+     }
+    func didEnd(_ contact: SKPhysicsContact) {
+        self.physicsWorld.contactDelegate = self //where self is a current scene
+    }
+
+  }
+
